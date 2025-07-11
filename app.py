a5fa0223a9ee5c91424b3f2d34a9c8319eae2796
Dhanush R Moolemane <152917673+dhanush-r-m@users.noreply.github.com>
2025-07-11 23:01:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler , LabelEncoder, OneHotEncoder
 import tensorflow as tf
 import pickle
